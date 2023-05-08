d1b79ee3c7daa287c0e222d897ca6e4d07f16415
Pytaichuk Misha <pytaichuk2020ki11@student.karazin.ua>
2023-05-09 00:07:49 +0300

    homework(simple web calculator)

--- src/main/java/ua/pytaichuk/controllers/FirstController.java
+++ src/main/java/ua/pytaichuk/controllers/FirstController.java
@@ -1,18 +1,54 @@
 package ua.pytaichuk.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(){
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model){
+
+
+        model.addAttribute("message", name + " " + surname);
+
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
     public String goodbyePage(){
         return "first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam("a") Double a,
+                             @RequestParam("b") Double b,
+                             @RequestParam("action") String action,
+                             Model model){
+        double res = 0.0;
+
+        switch (action){
+            case "multi":
+                res = a*b;
+                break;
+            case "add":
+                res = a+b;
+                break;
+            case "sub":
+                res = a-b;
+                break;
+            case "div":
+                res = a/b;
+                break;
+        }
+
+        model.addAttribute("result", res);
+        model.addAttribute("action", action);
+
+        return "first/calculator";
     }
 }
